a4a173971875b4a7af31b2f11c0bc58fb234da51
leomarcel <leo.marcel@epitech.eu>
2021-03-05 23:51:24 +0100

    add tools

---
+++ tools/wslHelp.bat
@@ -0,0 +1,13 @@
+@echo off
+@echo ------------------------------------------------------
+echo WSL HELP CMD
+@echo ------------------------------------------------------
+echo wsl --distribution Ubuntu           : Exec WSL
+echo wsl --list --running                : List WSL Running
+echo wsl --list --all                    : List All WSL
+echo wsl --terminate Ubuntu              : Stop WSL
+echo wslx                                : Start WSL X11
+echo wslIP                               : IP of WSL
+echo xslHELP                             : HELP
+echo -
+echo #Replace Ubuntu with your distribution
